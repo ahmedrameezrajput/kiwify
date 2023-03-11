7b9ea6e5f66df03ced2ad36766d85c7532f0b3f7
Ahmed Rameez <49881598+ahmedrameezrajput@users.noreply.github.com>
2023-03-11 23:38:32 +0500

    another commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="Login" element={<Login />} />
-        <Route path="Register" element={<Register />} />
-        <Route path="ForgotPassword" element={<ForgotPassword />} />
+        <Route path="/kiwify" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
       </Routes>
       <Login />
     </div>
